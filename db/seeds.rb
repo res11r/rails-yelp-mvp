a9663f2f3bf616b1c02ea490d586a009c2883492
Ahmed Albaiti <ahmedalbaiti1@gmail.com>
2022-02-16 14:28:28 +0400

    completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts 'Creating restaurants...'
+Restaurant.destroy_all
+5.times do
+  Restaurant.create!(name: Faker::Restaurant.name, address: Faker::Address.street_address, category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+end
+
+puts 'Seeds created'
